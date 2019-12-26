8d6ed1a3e308323e90db4acfca1ddf72612de370
Jeff Hernandez <12969559+jeff-hernandez@users.noreply.github.com>
2019-12-26 13:16:31 -0600

    Parameter Test

--- .github/actions/circleci-api/entrypoint.py
+++ .github/actions/circleci-api/entrypoint.py
@@ -1,5 +1,5 @@
 import circleci, github
-import subprocess
+# import subprocess
 from argparse import ArgumentParser
 
 if __name__ == '__main__':
@@ -7,5 +7,7 @@ if __name__ == '__main__':
     command.add_argument('--x0')
 
     args = command.parse_args()
-    subprocess.run(["echo", "x0: %s" % args.x0])
-    subprocess.run(["echo", "::set-output name=y0::%s" % args.x0])
+    print("x0: %s" % args.x0)
+    print("::set-output name=y0::%s" % args.x0)
+    # subprocess.run(["echo", "x0: %s" % args.x0])
+    # subprocess.run(["echo", "::set-output name=y0::%s" % args.x0])

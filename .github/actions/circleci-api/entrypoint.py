03c38eff61a2803489d493ceb7b9f99416e31e3c
Jeff Hernandez <12969559+jeff-hernandez@users.noreply.github.com>
2019-12-26 13:13:44 -0600

    Parameter Test

--- .github/actions/circleci-api/entrypoint.py
+++ .github/actions/circleci-api/entrypoint.py
@@ -1,9 +1,11 @@
 import circleci, github
+import subprocess
 from argparse import ArgumentParser
 
 if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser.add_argument('--username')
-    parser.add_argument('--project')
-    parser.add_argument('--token')
-    args = parser.parse_args()
+    command = ArgumentParser()
+    command.add_argument('--x0')
+
+    args = command.parse_args()
+    subprocess.run(["echo", "x0: %s" % args.x0])
+    subprocess.run(["echo", "::set-output name=y0::%s" % args.x0])

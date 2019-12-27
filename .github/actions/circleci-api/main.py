3989f303da242b448c041dd1b4c1e3ee1f43612e
Jeff Hernandez <12969559+jeff-hernandez@users.noreply.github.com>
2019-12-27 11:56:06 -0600

    Step Dev

--- .github/actions/circleci-api/main.py
+++ .github/actions/circleci-api/main.py
@@ -4,10 +4,13 @@ import scripts
 if __name__ == '__main__':
     task = argparse.ArgumentParser()
     task.add_argument('name')
-    task.add_argument('--repository')
+    task.add_argument('repository')
     task.add_argument('--circle-token')
     task = task.parse_args()
     
     if task.name == 'latest_workflow_status':
         succes = scripts.latest_workflow_status(task.repository, task.circle_token)
         assert succes, 'latest workflow was not successful'
+
+    else:
+        raise ValueError('task not supported')

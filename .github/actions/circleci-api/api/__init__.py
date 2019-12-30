bfa8a7827ff1d1ee8d3ff9932332fdc1febb8068
Jeff Hernandez <12969559+jeff-hernandez@users.noreply.github.com>
2019-12-30 14:23:49 -0600

    Check for Tests and Workflows

--- .github/actions/circleci-api/api/__init__.py
+++ .github/actions/circleci-api/api/__init__.py
@@ -14,20 +14,22 @@ def latest_commit(repository):
 def latest_workflow(repository, circle_token=''):
     url = "https://circleci.com/api/v1.1"
     url += "/project/github/{0}/tree/master"
-    url += "?circle-token={1}&limit=5&offset=5&filter=completed"
+    url += "?circle-token={1}&limit=5"
     response = requests.get(url.format(repository, circle_token))
 
     assert response.status_code == 200, response
-    json = response.json()
-    assert json, 'no workflows found'
+    integration_tests = response.json()
+    assert integration_tests, 'no integration tests found'
 
     records = []
-    for test in json:
+    for test in integration_tests:
+        if 'workflows' not in test: continue
         workflows = test['workflows']
         workflows['status'] = test['status']
         records.append(workflows)
 
     df, key = pd.DataFrame(records), 'workflow_id'
+    assert not df.empty, 'no workflows found'
     workflows, workflow_id = df.groupby(key, sort=False), df[key][1]
     workflow = workflows.get_group(workflow_id)
     return workflow

e45914ffe8e9c70376c5eba6e999e80ef66db2aa
Gaurav Sheni <gaurav.sheni@alteryx.com>
2020-07-02 17:30:05 -0400

    remove url encode for build

--- circleci/api.py
+++ circleci/api.py
@@ -48,8 +48,6 @@ def latest_workflow(repository, circle_token="", status="completed",
 def project_build(repository, circle_token="", branch=None):
     if branch is None:
         branch = default_branch(repository)
-        # CircleCI API requires url-encoded branch
-        branch = urllib.parse.quote(branch)
     url = CIRCLE_API
     url += f"/project/github/{repository}/build?circle-token={circle_token}"
     response = requests.post(url, data={"branch": branch})
